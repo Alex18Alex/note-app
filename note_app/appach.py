2e638b05d7044834d03f0f4f7ab852400344b333
Alex18Alex <Santz163@gmail.com>
2025-11-10 17:45:48 -0800

    correction

--- note_app/appach.py
+++ note_app/appach.py
@@ -117,4 +117,4 @@ if __name__ == '__main__':
         if not os.path.exists(log_file):
             open(log_file, 'w').close()
 
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
